4980d951847149e8e40e2e79bb3ad76f6a2a8593
Hamza <Hamza@phantomore.com>
2020-08-07 15:39:37 +0500

    [minor]

--- Flask/run.py
+++ Flask/run.py
@@ -1,103 +1,96 @@
-# import json
-# import plotly
-# import pandas as pd
-# import os
-# from nltk.stem import WordNetLemmatizer
-# from nltk.tokenize import word_tokenize
+import json
+import plotly
+import pandas as pd
+import os
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
+from sqlalchemy import create_engine
+import string
+import re
+import joblib
 
+def tokenize(text):
+    url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    text = re.sub(url_regex, "", text).lower().split()
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in text]
+    tokens = word_tokenize(' '.join(stripped))
+    lemmatizer = WordNetLemmatizer()
 
-
-
-# def tokenize(text):
-#     url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-#     text = re.sub(url_regex, "", text).lower().split()
-#     table = str.maketrans('', '', string.punctuation)
-#     stripped = [w.translate(table) for w in text]
-#     tokens = word_tokenize(' '.join(stripped))
-#     lemmatizer = WordNetLemmatizer()
-
-#     clean_tokens = []
-#     for tok in tokens:
-#         clean_tok = lemmatizer.lemmatize(tok)
-#         clean_tokens.append(clean_tok)
-#         return clean_tokens
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok)
+        clean_tokens.append(clean_tok)
+        return clean_tokens
 
 # load data
-# path = '..//data//DisasterResponse.db'
-# engine = create_engine('sqlite:///' + path)
-# df = pd.read_sql_table('DisasterResponse', engine)
+path = '..//data//DisasterResponse.db'
+engine = create_engine('sqlite:///' + path)
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-# model = joblib.load("../models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
-# @app.route('/hello,m')
-# @app.route('/index')
-# def index():
+@app.route('/hello,m')
+@app.route('/index')
+def index():
     
-#     # extract data needed for visuals
-#     # TODO: Below is an example - modify to extract data for your own visuals
-#     genre_counts = df.groupby('genre').count()['message']
-#     genre_names = list(genre_counts.index)
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
     
-#     # create visuals
-#     # TODO: Below is an example - modify to create your own visuals
-#     graphs = [
-#         {
-#             'data': [
-#                 Bar(
-#                     x=genre_names,
-#                     y=genre_counts
-#                 )
-#             ],
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
 
-#             'layout': {
-#                 'title': 'Distribution of Message Genres',
-#                 'yaxis': {
-#                     'title': "Count"
-#                 },
-#                 'xaxis': {
-#                     'title': "Genre"
-#                 }
-#             }
-#         }
-#     ]
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
     
-#     # encode plotly graphs in JSON
-#     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-#     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
-#     # render web page with plotly graphs
-#     return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
-# # web page that handles user query and displays model results
-# @app.route('/go')
-# def go():
-#     # save user input in query
-#     query = request.args.get('query', '') 
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
 
-#     # use model to predict classification for query
-#     classification_labels = model.predict([query])[0]
-#     classification_results = dict(zip(df.columns[4:], classification_labels))
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
 
-#     # This will render the go.html Please see that file. 
-#     return render_template(
-#         'go.html',
-#         query=query,
-#         classification_result=classification_results
-#     )
-
-
-# def main():
-#     app.run(host='0.0.0.0', port=3000, debug=True)
-#     tokenize(text)
-
-# if __name__ == '__main__':
-#     main()
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
 
 app = Flask(__name__)
 @app.route('/hello', methods=['POST'])
@@ -113,7 +106,7 @@ def hello():
     
 
 def main():
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
     
 
 if __name__ == '__main__':
